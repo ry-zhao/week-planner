4ca2bc165c73ee025e4d1c3c81d1fe6fb2a6efdd
Ryan Zhao <ryanzhao21@gmail.com>
2021-09-13 13:12:19 -0700

    In progress.

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 var $popUpAddEntry = document.querySelector('#popUpAddEntry');
 var $addEntry = document.querySelector('#addEntry');
+var $form = document.querySelector('form');
+
+var data = { entries: [] };
 
 $addEntry.addEventListener('click', showPopUp);
+$form.addEventListener('submit', addSubmission);
+
+function addSubmission(event) {
+  var entry;
+  entry.day = $form.elements.day;
+  entry.time = $form.elements.time;
+  entry.description = $form.elements.description;
+  localStorage.setItem();
+}
 
 function showPopUp(event) {
   $popUpAddEntry.className = 'dimmer container';
