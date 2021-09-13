24d3343c68034c9cf97581526f8b369175cd326f
Barbara Liao <barbara.liao3@gmail.com>
2021-09-13 14:40:49 -0700

    In progress.

--- main.js
+++ main.js
@@ -2,17 +2,33 @@ var $popUpAddEntry = document.querySelector('#popUpAddEntry');
 var $addEntry = document.querySelector('#addEntry');
 var $form = document.querySelector('form');
 
-var data = { entries: [] };
+var data = [];
 
 $addEntry.addEventListener('click', showPopUp);
 $form.addEventListener('submit', addSubmission);
 
+var previousEntriesJSON = localStorage.getItem('entry-list');
+
+if (previousEntriesJSON !== null) {
+  data = JSON.parse(previousEntriesJSON);
+}
+
+function handleUnload(event) {
+  var entriesJSON = JSON.stringify(data);
+  localStorage.setItem('entry-list', entriesJSON);
+}
+
+window.addEventListener('beforeunload', handleUnload);
+
 function addSubmission(event) {
-  var entry;
-  entry.day = $form.elements.day;
-  entry.time = $form.elements.time;
-  entry.description = $form.elements.description;
-  localStorage.setItem();
+  event.preventDefault();
+  var entry = {};
+  entry.day = $form.elements.day.value;
+  entry.time = $form.elements.time.value;
+  entry.description = $form.elements.description.value;
+  data.push(entry);
+  $popUpAddEntry.className = 'hidden dimmer container';
+  $form.reset();
 }
 
 function showPopUp(event) {
